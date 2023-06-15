d2e3a1a655e4fb7f7e3dddc6335b4a99d6b79a7d
Kellan Anderson <kellandanderson@gmail.com>
2023-06-15 14:22:06 -0400

    Profile page w/ ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,8 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { filterUserForClient } from "~/server/helper/filterUserForClient";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
@@ -19,10 +19,6 @@ const ratelimit = new Ratelimit({
    */ 
   prefix: "@upstash/ratelimit",
 });
-
-const filterUserForClient = (user: User) => {
-  return { id: user.id, username: user.username, profilePictureURL: user.profileImageUrl }
-}
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
